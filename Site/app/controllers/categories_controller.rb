2ac751ea321fe2dd6df135f026538fcbaf239dfd
andrew <github email address>
2017-03-04 09:37:45 +0200

    create categories_controller.rb

--- Site/app/controllers/categories_controller.rb
+++ Site/app/controllers/categories_controller.rb
@@ -1,1 +1,41 @@
-categories_controller.rb
+class CategoriesController < ApplicationController
+
+	def index
+		@categories = Category.all
+		render json: @categories
+	end
+
+	def show
+		@category = Category.find(params[:id])
+		render json: @category
+	end
+
+	def create
+		@category = Category.new(categories_param)
+		render json: @category
+	end
+
+	def edit
+		@category = Category.find_by(params[:id])
+		render json: @category
+	end
+
+	def update
+		@category = Category.find_by(params[:id])
+		@category.update(categories_params)
+		render json: @category
+	end
+
+	def destroy
+		@category = Category.find_by(params[:id])
+		@category.destroy
+		render json: @category
+	end
+
+	private
+
+	def categories_params
+		params.require(:category).permit(:name)
+	end 		
+
+end
