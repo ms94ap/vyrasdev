77c147b6758114ac0d266ab4f356430bfdebfa44
andrew <github email address>
2017-03-05 10:56:43 +0200

    start routes

--- API/config/routes.rb
+++ API/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pages do
+  end
+
+  resources :posts do
+  end
+
+  resources :categories do
+  end
+
+  resources :tags do
+  end
+
 end
